7eb8295f7496042bc25e30b5257103f0792c6d59
lienquan <lienquan1988@163.com>
2018-10-14 10:54:03 +0800

    aviator

--- aviator/src/test/java/com/lien/TestAviator.java
+++ aviator/src/test/java/com/lien/TestAviator.java
@@ -1,6 +1,10 @@
 package com.lien;
 
 import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.runtime.function.FunctionUtils;
+import com.googlecode.aviator.runtime.type.AviatorDouble;
+import com.googlecode.aviator.runtime.type.AviatorFunction;
+import com.googlecode.aviator.runtime.type.AviatorObject;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -24,8 +28,14 @@ public class TestAviator {
     @Test
     public void testStringEnv(){
         Map<String,Object> env = new HashMap<>();
-        env.put("yourName", "lien");
+        env.put("yourName", "test");
         String result = (String) AviatorEvaluator.execute("'hello ' + yourName", env);
         System.out.println(result);
     }
+
+    @Test
+    public void testFunction(){
+        System.out.println(AviatorEvaluator.execute("string.contains(\"test\",string.substring('hello',1,2))"));
+    }
+
 }
