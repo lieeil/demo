c23fbf21e0ba85271fa5b912e619bac94bce25c3
lienquan <lienquan1988@163.com>
2018-10-29 19:10:12 +0800

    多线程问题

---
+++ basic/src/main/java/com/lien/threads/ProductDemo.java
@@ -0,0 +1,41 @@
+package com.lien.threads;
+
+/**
+ * Description: <br>
+ *
+ * @author li
+ * @date 2018/10/17 上午10:06
+ */
+public class ProductDemo {
+
+    private Integer id;
+
+    private String name;
+
+    private String desc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
--- basic/src/main/java/com/lien/threads/ThreadExecutorService.java
+++ basic/src/main/java/com/lien/threads/ThreadExecutorService.java
@@ -1,11 +1,9 @@
 package com.lien.threads;
 
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -31,7 +29,7 @@ public class ThreadExecutorService {
         return executorService;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Product> productList = new ArrayList<>();
         for(int i = 0; i < 10; i++){
             Product product = new Product();
@@ -40,26 +38,42 @@ public class ThreadExecutorService {
             product.setDesc("第" + i + "个");
             productList.add(product);
         }
-        List<Future> futures = new ArrayList<>();
+        List<Future<ProductDemo>> futures = new ArrayList<>();
         for(Product product : productList){
-            Future future = ThreadExecutorService.createPool().submit(new Runnable() {
-                @Override
-                public void run() {
+            Future<ProductDemo> future = ThreadExecutorService.createPool().submit(new Callable<ProductDemo>() {
+
+                public ProductDemo call() {
+                    ProductDemo productDemo = new ProductDemo();
                     System.out.println("休眠前:" + Thread.currentThread().getName() + " id="
                           + product.getId() + " name=" +product.getName()
                           + " desc=" + product.getDesc());
+                    product.setName(product.getName() + "+" + product.getId());
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(10000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     System.out.println("休眠后:" + Thread.currentThread().getName() + " id="
                             + product.getId() + " name=" +product.getName()
                             + " desc=" + product.getDesc());
+                    productDemo.setId(product.getId());
+                    productDemo.setName(product.getName());
+                    productDemo.setDesc(product.getDesc());
+                    return productDemo;
                 }
             });
             futures.add(future);
         }
+        List<ProductDemo> productDemoList = new ArrayList<>();
+        for(Future<ProductDemo> future : futures){
+            productDemoList.add(future.get());
+        }
+        for(ProductDemo demo : productDemoList){
+            System.out.println(" id="
+                    + demo.getId() + " name=" +demo.getName()
+                    + " desc=" + demo.getDesc());
+        }
+
     }
 
 }
