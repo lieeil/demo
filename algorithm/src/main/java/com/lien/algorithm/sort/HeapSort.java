5b953bd823a395c1806ee820886a237811cfa11e
lienquan <lienquan1988@163.com>
2019-01-20 10:03:45 +0800

    堆排序

--- algorithm/src/main/java/com/lien/algorithm/sort/HeapSort.java
+++ algorithm/src/main/java/com/lien/algorithm/sort/HeapSort.java
@@ -11,19 +11,57 @@ public class HeapSort extends Sort {
 
     @Override
     void sort() {
-        for(int i = items.length -1; i >= 0; i--){
-            headSort(i);
+        //建堆
+        for(int i = items.length - 1; i >= 0; i--){
+            shiftDown(i, items.length -1);
+        }
+        swap(0,items.length - 1);
+        for(int i = items.length - 2; i >= 0; i--){
+            shiftDown(0, i);
+            swap(0,i);
         }
     }
 
-    private void headSort(int length){
-        for(int i = length; i >= 0; i--){
-            int noLeafIndex = (i-1)/2;
-            int leftIndex = noLeafIndex*2 + 1;
-            int rightIndex = noLeafIndex*2 + 2;
-            if(items[noLeafIndex] < items[rightIndex] && items[noLeafIndex] < items[leftIndex]){
-                swap(noLeafIndex, items[leftIndex] < items[rightIndex] ? items[rightIndex]:items[leftIndex]);
+
+
+    /**
+     * 节点下移操作
+     * @param parentIndex
+     * @param lastIndex
+     */
+    private void shiftDown(int parentIndex, int lastIndex){
+        while(parentIndex <= lastIndex && 2*parentIndex + 1 <= lastIndex){
+            int leftSonIndex = 2*parentIndex + 1;
+            int rightSonIndex = 2*parentIndex + 2;
+            int targetIndex = -1;
+            if(rightSonIndex <= lastIndex){
+                if(items[parentIndex] < items[leftSonIndex] || items[parentIndex] < items[rightSonIndex]){
+                    if(items[leftSonIndex] > items[rightSonIndex]){
+                        targetIndex = leftSonIndex;
+                    }else{
+                        targetIndex = rightSonIndex;
+                    }
+                }
+            }else {
+                if(items[parentIndex] < items[leftSonIndex]){
+                    targetIndex = leftSonIndex;
+                }
+            }
+            if(targetIndex != -1){
+                swap(parentIndex, targetIndex);
+                parentIndex = targetIndex;
+            }else{
+                break;
             }
         }
     }
+
+    public static void main(String[] args){
+        HeapSort sort = new HeapSort();
+        Integer[] items = {99,3009,45,34985,12,45,67,1,234,56,8890,7864};
+        sort.setItems(items);
+        sort.sort();
+        sort.printItems();
+    }
+
 }
